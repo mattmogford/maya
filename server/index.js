7f465a0d9615a3494803427b234c87a303973480
Matthew Mogford <matthewmogford@kukdlt022.eurofoods.local>
2018-09-13 10:48:01 +0100

    Use http on express server, but also ready code for https

--- server/index.js
+++ server/index.js
@@ -2,8 +2,22 @@ var express = require('express')
 var app = express()
 const path = require('path')
 
+const http = require('http')
+// const https = require('https')
+
+const port = parseInt(process.env.PORT, 10) || 3000
+// const sslPort = parseInt(process.env.PORT, 10) || 3001
+
 app.use(express.static(`${__dirname}/../build`))
-app.listen(3000)
+
+http.createServer(app).listen(port, (err) => {
+    if (err) throw err
+    console.log(`> Ready on http://localhost:${port}`)
+})
+// https.createServer(sslOptions, expressApp).listen(sslPort, (err) => {
+//     if (err) throw err
+//     console.log(`> Ready on https://localhost:${sslPort}`)
+// })
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../build/index.html'));
