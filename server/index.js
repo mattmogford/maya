34478a75872a3583be944033fd25d463a1a3d2ce
Matthew Mogford <matthewmogford@kukdlt022.eurofoods.local>
2018-09-13 10:33:42 +0100

    updated express server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,10 @@
 var express = require('express')
 var app = express()
+const path = require('path')
 
-app.use(express.static(__dirname + './../../build/'))
+app.use(express.static(`${__dirname}/../build`))
 app.listen(3000)
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build/index.html'));
+})
